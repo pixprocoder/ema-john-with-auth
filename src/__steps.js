72a21f846a664d408d60497cf1a5d40330055fb2
kabir <makkobir79@gmail.com>
2022-06-21 05:24:51 +0600

    initilize firebase again

---
+++ src/__steps.js
@@ -0,0 +1,8 @@
+/**
+ * firebase setup steps
+ * --------------------------
+ * 1. create a new project >console.firebase.google.com
+ * 2. install firebase
+ * 3. copy and past sdk text into firebase.init.js file
+ * 4. Enable auth provider from your project setting
+ */
