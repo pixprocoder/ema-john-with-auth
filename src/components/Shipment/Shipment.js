123d99e6d463da0276f3508c5048a2a1db6b6d36
kabir <makkobir79@gmail.com>
2022-06-21 18:43:27 +0600

    tailwind added

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,9 +1,86 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../Firebase.init";
 
 const Shipment = () => {
+  const [user] = useAuthState(auth);
   return (
     <div>
-      <h1>Add your information</h1>
+      <h1 className="text-center text-5xl text-blue-500  my-9">
+        Add your information
+      </h1>
+
+      <div className="flex items-center justify-center">
+        <div className="mx-auto w-full max-w-[550px]">
+          <form>
+            <div className="mb-5">
+              <label
+                for="name"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                Full Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Full Name"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              />
+            </div>
+            <div className="mb-5">
+              <label for="email" className="mb-3 block font-medium ">
+                Email Address
+              </label>
+              <input
+                value={user.email}
+                type="email"
+                name="email"
+                id="email"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium  outline-none"
+                readOnly
+                autoComplete="off"
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                for="subject"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                Address
+              </label>
+              <input
+                type="text"
+                name="subject"
+                id="subject"
+                placeholder="Enter your Address"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                for="subject"
+                className="mb-3 block text-base font-medium text-[#07074D]"
+              >
+                Phone
+              </label>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                placeholder="Enter your Phone"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              />
+            </div>
+
+            <div>
+              <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
